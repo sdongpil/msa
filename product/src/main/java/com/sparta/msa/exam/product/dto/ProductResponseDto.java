64a4e9d2578bde76730264f03ecb18e46f62a5d8
sdongpil <pildong94@naver.com>
2024-12-03 00:28:34 +0900

    feat: 상품 등록 및 조회 API Redis 캐시 적용
    - 상품 조회 페이징 처리 및 캐시 적용
    - 상품 등록 -> 상품 조회 캐시 초기화

--- product/src/main/java/com/sparta/msa/exam/product/dto/ProductResponseDto.java
+++ product/src/main/java/com/sparta/msa/exam/product/dto/ProductResponseDto.java
@@ -1,5 +1,10 @@
 package com.sparta.msa.exam.product.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.sparta.msa.exam.product.domain.ProductStatus;
 import com.sparta.msa.exam.product.domain.entity.Product;
 import lombok.Builder;
@@ -12,6 +17,9 @@ public record ProductResponseDto(Long id,
                                  int supplyPrice,
                                  ProductStatus productStatus,
                                  int stock,
+                                 @JsonSerialize(using = LocalDateTimeSerializer.class)
+                                 @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+                                 @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")
                                  LocalDateTime createdAt
 ) {
 
