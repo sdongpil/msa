469766cc8f70d7388fcc85a36a11de05f7c44c8a
sdongpil <pildong94@naver.com>
2024-12-03 17:52:13 +0900

    refactor: 패키지 분리

---
+++ order/src/main/java/com/sparta/msa/exam/order/service/validator/OrderStockValidatorImplV1.java
@@ -0,0 +1,54 @@
+package com.sparta.msa.exam.order.service.validator;
+
+import com.sparta.msa.exam.order.domain.entity.OrderStatus;
+import com.sparta.msa.exam.order.domain.entity.order.Order;
+import com.sparta.msa.exam.order.domain.repository.OrderRepository;
+import com.sparta.msa.exam.order.dto.OrderProductInfoDto;
+import com.sparta.msa.exam.order.dto.OrderRequestDto;
+import com.sparta.msa.exam.order.dto.StockReservationRequestDto;
+import com.sparta.msa.exam.order.service.ProductClient;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class OrderStockValidatorImplV1 implements OrderStockValidator {
+    private final ProductClient productClient;
+    private final OrderRepository orderRepository;
+
+    @Override
+    public boolean commit(String transactionId) {
+        return productClient.commitStockReservation(transactionId);
+    }
+
+    @Override
+    public void reserveProductStocks(OrderRequestDto requestDto, String transactionId) {
+        List<OrderProductInfoDto> orderProductInfoDtoList = requestDto.orderProductList();
+        for (OrderProductInfoDto item : orderProductInfoDtoList) {
+            boolean prepareStock = productClient.prepareStockReservation(item.getProductId(), new StockReservationRequestDto(item.getQuantity(), transactionId));
+
+            if (!prepareStock) {
+                throw new IllegalStateException("재고 문제 발생");
+            }
+        }
+    }
+
+    @Override
+    @Transactional
+    public void rollbackCommittedStock(String transactionId) {
+        productClient.rollbackCommittedStock(transactionId);
+
+        Order order = orderRepository.findByTransactionId(transactionId);
+        order.setStatus(OrderStatus.FAIL);
+    }
+
+    @Override
+    public void redisDelete(String transactionId) {
+        productClient.redisDelete(transactionId);
+    }
+}
